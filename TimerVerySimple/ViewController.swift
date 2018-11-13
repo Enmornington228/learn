200483343e5ec7dcfb8761d1d16bfb23f17c5ed5
UPTech Team <uptechteam@uptechs-macbook-air.local>
2018-11-13 16:00:34 +0200

    Initial Commit

---
+++ TimerVerySimple/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimerVerySimple
+//
+//  Created by UPTech Team on 11/13/18.
+//  Copyright © 2018 UPTech Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
